829bb41a411339df7e93f1c5e985c927410f4c1a
Sam Merwan <sam.mjalaoui@gmail.com>
2024-09-04 16:04:58 +0200

    update routes for messages

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -1,14 +1,66 @@
 const express = require('express');
 const router = express.Router();
 const Message = require('../models/Message.model');
+const User = require('../models/User.model');
 
 router.post('/', async (req, res, next) => {
     try {
-      const savedMessage = await Message.create(req.body);
-      res.status(201).json(savedMessage);
+        const savedMessage = await Message.create(req.body);
+
+        // format the same why at the get to update the chat
+        const formattedMessage = {
+            ...savedMessage.toObject(),
+            from: { _id: savedMessage.from },
+            to: { _id: savedMessage.to }
+        };
+
+        res.status(201).json(formattedMessage);
     } catch (error) {
-      next(error)
+        next(error);
     }
 });
+
+router.get('/', async (req, res, next) => {
+    const authenticatedUserId = req.payload._id;
+    const otherUserId = req.query.userId;
+
+    try {
+        const messages = await Message.find({
+            $or: [
+                { from: authenticatedUserId, to: otherUserId },
+                { from: otherUserId, to: authenticatedUserId }
+            ]
+        })
+        .populate({
+            path: 'from',
+            select: '_id'
+        })
+        .populate({
+            path: 'to',
+            select: '_id'
+        })
+        .sort({ createdAt: 1 });
+
+        res.json(messages);
+    } catch (error) {
+        next(error);
+    }
+});
+  
+// get all user names should be update later to return only the users for who a message already exist
+// should return the availability too
+router.get('/chats', async (req, res, next) => {
+    try {
+        const users = await User.find().select('_id name');
+        const formattedUsers = users.map(user => ({
+            _id: user._id,
+            username: user.name
+        }));
+        res.json(formattedUsers);
+    } catch (error) {
+        next(error);
+    }
+});
+
 
 module.exports = router;
